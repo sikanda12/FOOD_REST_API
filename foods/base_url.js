8fcee63342ed2de107be752d5fa0480c857b1a5b
sikandar pandit <47684922+sikanda12@users.noreply.github.com>
2023-04-29 17:10:21 +0530

    Update base_url.js

--- foods/base_url.js
+++ foods/base_url.js
@@ -1,5 +1,5 @@
 const BASE_URL='https://foodapi-lqod.onrender.com/';
-const uploadDir='uploads/';
+const uploadDir='public/uploads/';
 module.exports = BASE_URL+uploadDir;
 
 console.log('BASE URL is Ready to Use');
